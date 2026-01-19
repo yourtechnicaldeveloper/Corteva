9086d69f28485d610cb700b77048c0ae658396a9
yourtechnicaldeveloper <yourtechnicaldeveloper@gmail.com>
2026-01-19 19:46:23 +0530

    remove ngrok url

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,8 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (app()->environment('local')) {
-            URL::forceScheme('https');
-        }
+        // if (app()->environment('local')) {
+        //     URL::forceScheme('https');
+        // }
     }
 }
